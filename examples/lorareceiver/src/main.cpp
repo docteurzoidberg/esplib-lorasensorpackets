d0e673173828de92780cd0e6deabc8b6db5d4312
DrZoid <docteurzoidberg@users.noreply.github.com>
2020-09-22 00:46:48 +0200

    debug receiver

--- examples/lorareceiver/src/main.cpp
+++ examples/lorareceiver/src/main.cpp
@@ -21,11 +21,52 @@
 uint16_t packetCounter=0;
 uint32_t lastPacketReceived=0;
 
+void print_byte(uint8_t byte) {
+    static std::array<char, 16> to_hex = {
+        '0', '1', '2', '3',
+        '4', '5', '6', '7',
+        '8', '9', 'A', 'B',
+        'C', 'D', 'E', 'F'
+    };
+
+    Serial.print(to_hex[(byte >> 4) & 0xF]);
+    Serial.print(to_hex[(byte >> 0) & 0xF]);
+}
+
+void print_buffer(uint8_t const* buffer, std::size_t size) {
+    std::size_t per_line = 16;
+    std::size_t line_count = (size + per_line - 1) / per_line;
+
+    for(int line = 0; line < line_count; ++line) {
+        if (line > 0) {
+            Serial.print('\n');
+        }
+
+        std::size_t byte_count = line == line_count - 1 ? size - line * per_line : per_line;
+
+        for(int i = 0; i < byte_count; ++i) {
+            int idx = line * per_line + i;
+            uint8_t byte = buffer[idx];
+            print_byte(byte);
+            Serial.print(' ');
+        }
+    }
+}
+
+void print_packet_content(dzb::Packet const& packet) {
+  dzb::PacketReader reader(packet);
+  for(auto payload : reader.packets) {
+    Serial.printf("%s\n", dzb::name_of_type(dzb::deserialize<dzb::PacketType>(payload)));
+  }
+}
+
 void on_lora_packet_received(int size) {
   std::vector<uint8_t> buffer(size);
   LoRa.readBytes(buffer.data(), size);
+  print_buffer(buffer.data(), size);
 
   auto packet = dzb::Packet::deconstruct(buffer.data(), size);
+  print_packet_content(packet);
 
   if (!packet.is_crc_valid()) {
     // TODO: handle error
@@ -37,6 +78,8 @@ void on_lora_packet_received(int size) {
   bool presence=0;
   bool gpio_d1=0;
   bool gpio_d2=0;
+
+
 
   dzb::PacketReader reader(packet);
 
